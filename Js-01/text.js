647c3c291fc36b2aee6d228b24be0f0b730a4af3
KhizarHijazi <135581185+KhizarHijazi@users.noreply.github.com>
2024-09-04 09:34:46 +0000

    some changes

--- Js-01/text.js
+++ Js-01/text.js
@@ -1,1 +1,14 @@
-console.log("Khizar Hijazi")
+const myname = "Khizar Hijaz";
+let phone = 1234567890;
+var address = "world country city street and house No:22";
+job = "coder";
+
+let person = {
+    name : "Khizar Hijaz",
+    phone : 1234567890,
+    address: "world country city street and house No:22",
+    job : "coder",
+}
+console.log(person)
+// console.table(myname, phone , address, job )
+console.table([myname , phone, address , job])
